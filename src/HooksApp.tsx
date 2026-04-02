fdaad44a239b9dd288db82f17a3cd1799bd412be
Jorgemart313 <jorgemart3130@gmail.com>
2026-04-02 17:36:26 -0300

    final commit

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -1,7 +1,10 @@
 export const HooksApp = () => {
   return (
     <div className="bg-gradient">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! Bienvenido a mi web de aprendizaje con hooks, puedes probar
+        cualquiera de ellos y divertite un poco con estas tareitas de mi curso
+      </h1>
     </div>
   );
 };
